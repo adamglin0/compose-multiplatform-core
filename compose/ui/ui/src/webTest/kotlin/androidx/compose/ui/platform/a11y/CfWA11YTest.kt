403f5ed8df2c17795500b8a87185a808719be564
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2026-09-21 13:16:50 +0200

    Add a test for mergeDescendants=true on web (#3433)
    Adds a test for
    https://youtrack.jetbrains.com/issue/CMP-8624/Web-A11y.-mergeDescendants-true-doesnt-work-correctly
    
    ## Testing
    - Added a new test
    
    ## Release Notes
    N/A

--- compose/ui/ui/src/webTest/kotlin/androidx/compose/ui/platform/a11y/CfWA11YTest.kt
+++ compose/ui/ui/src/webTest/kotlin/androidx/compose/ui/platform/a11y/CfWA11YTest.kt
@@ -23,6 +23,7 @@ package androidx.compose.ui.platform.a11y
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -114,6 +115,29 @@ class CfWA11YTest : OnCanvasTests {
             button1.click()
             assertEquals(it + 1, clickCounter)
         }
+    }
+
+    @Test
+    fun mergeDescendantsMergesText() = runApplicationTest {
+        createComposeWindow {
+            Column(
+                modifier = Modifier
+                    .testTag("mergedItem")
+                    .semantics(mergeDescendants = true) {}
+            ) {
+                Row { Text("Hello") }
+                Row { Text("World") }
+            }
+        }
+
+        awaitA11YChanges()
+
+        val mergedNode = assertNotNull(
+            getShadowRoot().getElementById("mergedItem") as? HTMLElement,
+            "The merged semantics node must be present in the a11y tree",
+        )
+        assertEquals("Hello\nWorld", mergedNode.textContent)
+        assertEquals(0, mergedNode.children.length, "Merged text must not expose child nodes")
     }
 
     @Test
